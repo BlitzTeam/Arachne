4717d2e83f77d65e55acb8368f5b4ec5da86aee2
0xg <baptiste.angles@gmail.com>
2013-02-22 18:34:46 +0100

    Improve random selection by adding weights

--- genetic.py
+++ genetic.py
@@ -26,19 +26,28 @@ def evolve(pop, res):
 	generation = zip(pop, res)
 	generation.sort(key=operator.itemgetter(1))
 	newGeneration = []
-	
+	weightSum = sum([j for i,j in generation])
+
 	for i in range(len(pop)):
-		ra = random.randint(0,len(pop))
-		rb = random.randint(0,len(pop))
-		newGeneration.append(mutate(generation[ra], generation[rb]))
+		newGeneration.append(mutate(weightedRandomChoice(generation, weightSum), weightedRandomChoice(generation, weightSum))
 
 	return newGeneration
+
+def weightedRandomChoice(generation, weightSum):	
+	rand = random.random() * weightSum
+	for j in pop:
+		rand -= j[1]
+		if rand < 0:
+			return j
+
+
 
 def mutate(a, b):
 	a_score = a[1]
 	b_score = b[1]
 	a = a[0]
 	b = b[0]
+	
 	return ServoMotion((a.amplitude + b.amplitude)/2, (a.period + b.period)/2, (a.center + b.center)/2)
 
 if __name__ == '__main__':
