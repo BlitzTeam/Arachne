9f96d6526c76f89dcb2fd0e12e5a5fb5ba2aa5b0
eoj <baptiste.angles@gmail.com>
2013-02-22 15:54:40 +0100

    Implement a basic genetic algorithm

--- genetic.py
+++ genetic.py
@@ -1,4 +1,5 @@
 import random
+import operator
 from Motion import *
 
 def initPopulation(count = 100):
@@ -22,8 +23,23 @@ def fitnessFunction(gene):
 	return 1.0 # TODO
 
 def evolve(pop, res):
-	bestScore = max(res)
+	generation = zip(pop, res)
+	generation.sort(key=operator.itemgetter(1))
+	newGeneration = []
+	
+	for i in range(len(pop)):
+		ra = random.randint(0,len(pop))
+		rb = random.randint(0,len(pop))
+		newGeneration.append(mutate(generation[ra], generation[rb]))
 
+	return newGeneration
+
+def mutate(a, b):
+	a_score = a[1]
+	b_score = b[1]
+	a = a[0]
+	b = b[0]
+	return ServoMotion((a.amplitude + b.amplitude)/2, (a.period + b.period)/2, (a.center + b.center)/2)
 
 if __name__ == '__main__':
 	generationCount = 100
