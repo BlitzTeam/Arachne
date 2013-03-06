d502a6726f26bbbfb00b418b9d6493af03bb1915
eoj <baptiste.angles@gmail.com>
2013-03-06 16:00:55 +0100

    Update Leg to fix function positionToAngle

--- Leg.py
+++ Leg.py
@@ -1,6 +1,7 @@
 import numpy
 import sys, time
 import pydyn.dynamixel as dyn
+import math
 
 class Leg:
 	a1 = 4.9
@@ -41,8 +42,7 @@ class Leg:
 		 #u = a1 + b * numpy.cos(beta) + c * numpy.cos(beta+gamma)
 		 
 		#calcul de alpha
-		u = sqrt(x**2+y**2)
-		assert(
+		u = math.sqrt(x**2+y**2)
 		if (u != 0):
 			if(u < 0):
 				signe = -1
@@ -52,17 +52,16 @@ class Leg:
 		else:
 			raise AssertionError("error, alpha equals 0")
 			
-			
 		#calcul de gamma
-		tmpGamma = ((u - a1)**2 + (z + a2)**2 - b**2 - c**2)/2*b*c
-		if ( ((u - a1)**2 + (z - a2)**2) <= ((b+c)**2) && ( (((u -a1)**2) + (z - a2)**2) >= ( (b+c)**2) ) ):
+		tmpGamma = ((u - self.a1)**2 + (z + self.a2)**2 - self.b**2 - self.c**2) / 2 * self.b * self.c
+		if (((u - self.a1)**2 + (z - self.a2)**2) <= ((self.b + self.c)**2) and ((((u - self.a1)**2) + (z - self.a2)**2) >= ((self.b + self.c)**2))):
 			gamma = numpy.arccos(tmpGamma)
 		else:
 			raise AssertionError("error, gamma not between -1 & 1")
 		
 		#calcul de beta
-			sinBeta = ( (z-a2)*(b+c*numpy.cos(gamma)) - (u - a1) * c * numpy.sin(gamma) )/( (u-a1)**2+(z-a2)**2 )
-			cosBeta = ( (u-a1)*(b+c*numpy.cos(gamma)) - (z - a2) * c * numpy.sin(gamma) )/( (u-a1)**2+(z-a2)**2 )
+			sinBeta = ((z - self.a2) * (self.b + self.c * numpy.cos(gamma)) - (u - self.a1) * self.c * numpy.sin(gamma)) / ((u - self.a1)**2 + (z - self.a2)**2)
+			cosBeta = ((u - self.a1) * (self.b + self.c * numpy.cos(gamma)) - (z - self.a2) * self.c * numpy.sin(gamma)) / ((u - self.a1)**2 + (z - self.a2)**2)
 		
 		print "alpha : %f" % (alpha, )
 		print "cosBeta : " + cosBeta
